7835352608b9f1ff60ea88f3eee8acba4a1b5e5d
Ben Tillman <ben.tillman@gmail.com>
2012-11-10 23:12:30 +1300

    improve input handling

--- solver.rb
+++ solver.rb
@@ -1,38 +1,36 @@
-while true
-  gets.split("\n").each do |puzzle|
+while puzzle = gets
 
-    def check(puzzle, x, c)
-      ![row(puzzle, x), column(puzzle, x), block(puzzle, x)].join.include?(c)
-    end
-
-    def row(puzzle, x)
-      puzzle.slice(x/9*9, 9)
-    end
-
-    def column(puzzle, x)
-      (0..8).map {|i| puzzle[i*9+x%9]}
-    end
-
-    def block(puzzle, x)
-      (0..2).map {|i| puzzle.slice((i*9)+(x/27*27)+(x%9/3*3), 3)}.join
-    end
-
-    def solve(solution)
-      if x = solution =~ /0/
-        ("1".."9").each do |c|
-          if check(solution, x, c)
-            solution[x] = c
-            # puts "#{solution}:#{x}:#{c}"
-            return solution if solve(solution)
-          end
-        end
-        solution[x] = "0"
-        false
-      else
-        solution
-      end
-    end
-
-    puts solve(puzzle)
+  def check(puzzle, x, c)
+    ![row(puzzle, x), column(puzzle, x), block(puzzle, x)].join.include?(c)
   end
+
+  def row(puzzle, x)
+    puzzle.slice(x/9*9, 9)
+  end
+
+  def column(puzzle, x)
+    (0..8).map {|i| puzzle[i*9+x%9]}
+  end
+
+  def block(puzzle, x)
+    (0..2).map {|i| puzzle.slice((i*9)+(x/27*27)+(x%9/3*3), 3)}.join
+  end
+
+  def solve(puzzle)
+    if x = puzzle =~ /0/
+      ("1".."9").each do |c|
+        if check(puzzle, x, c)
+          puzzle[x] = c
+          # puts "#{puzzle}:#{x}:#{c}"
+          return puzzle if solve(puzzle)
+        end
+      end
+      puzzle[x] = "0"
+      false
+    else
+      puzzle
+    end
+  end
+
+  puts solve(puzzle)
 end
